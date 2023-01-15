aa7054123f48e9588899db4e34ffdc8723888d1c
Ben B <fellowsben@outlook.com>
2023-01-15 20:14:29 +0000

    Update user password

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -58,4 +58,25 @@ class UserController extends Controller
 
         return redirect('/login');
     }
+
+    // Update user password
+    public function UserUpdatePassword(Request $request) {
+        $request->validate([
+            'old_password' => 'required',
+            'new_password' => 'required|confirmed', 
+        ]);
+
+        // Check if the old password inputted is correct
+        if (!Hash::check($request->old_password, auth::user()->password)) {
+            return back()->with("error", "Old Password Does Not Match.");
+        }
+
+        // Update the user's password
+        User::whereId(auth()->user()->id)->update([
+            'password' => Hash::make($request->new_password)
+
+        ]);
+        return back()->with("status", "Password Successfully Updated.");
+
+    } 
 }
